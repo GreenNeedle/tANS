e99fd1ea1d3f9cbc761aeb8b0a73f6b710359535
Joanna <asia1008@poczta.onet.pl>
2017-09-10 17:32:27 +0200

    nb_bits_calculator: divided nb ROM and nb_bits calculation

--- tANS.srcs/sources_1/new/nb_bits_calculator.v
+++ tANS.srcs/sources_1/new/nb_bits_calculator.v
@@ -2,31 +2,12 @@
 
 
 module nb_bits_calculator(
-    input [7:0] symbol,
     input [7:0] state,
+    input [7:0] nb,
     output [3:0] nb_bits
     );
     
     parameter r = 5;
-    
-    reg [7:0] nb;
-    
-    always @ (symbol or state) begin
-        case (symbol)
-            8'b00110000 : begin
-                nb = 8'b01001000;
-            end
-            8'b00110001 : begin
-                nb = 8'b00010000;
-            end
-            8'b00110010 : begin
-                nb = 8'b00101100;
-            end
-            default : begin
-                nb = 8'b00000000;
-            end
-        endcase
-    end
     
     assign nb_bits = (state + nb) >> r;
     
